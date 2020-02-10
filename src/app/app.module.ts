4bb35b1610cb3ebaa70cfedacfc2a6d6401d18cd
robi1467 <robi1467@morris.umn.edu>
2020-02-10 07:20:40 -0600

    Created cart components and updated module file imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartService } from './cart.service';
+import {  CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -25,7 +26,8 @@ import { CartService } from './cart.service';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartService
+    CartComponent,
+  
   ],
   bootstrap: [ AppComponent ]
 })
